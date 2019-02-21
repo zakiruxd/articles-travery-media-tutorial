b1e15b06f77b3853ccf9df2b261f8771fcea6d63
Zakir Faizal <zakiruxd@gmail.com>
2019-02-20 21:13:13 -0500

    first commit

--- app.js
+++ app.js
@@ -96,7 +96,10 @@ let users = require("./routes/users");
 app.use("/users", users);
 
 //Listener
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("Server has started");
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
